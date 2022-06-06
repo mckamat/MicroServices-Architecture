06e17ca72f44a4bb483e4e5c9011fd95f8ca6e97
Mücahit Kamat <mucahit.kamat@dgpaysit.com>
2022-06-06 17:43:43 +0300

    Add project files.

---
+++ Shared/FreeCourse.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace FreeCourse.Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; set; }
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+    }
+}
